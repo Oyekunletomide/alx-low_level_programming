e621b07c852fbf415e8b833ab7f227bc32846662
tomide <oyekunletomide@gmail.com>
2022-07-01 16:25:32 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Prints lowercase letters of the alphabet in reverse using putchar()
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+	char alpha[] = "abcdfghijklmnoprstuvwxyz\0";
+	int i;
+
+	for (i = 0; alpha[i] != '\0'; i++)
+	putchar(alpha[i]);
+	putchar('\n');
+	
+	return (0);
+}
