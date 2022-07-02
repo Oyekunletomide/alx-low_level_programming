75b56ebea8f103f3130e844c50b5536c92479598
tomide <oyekunletomide@gmail.com>
2022-07-01 18:22:58 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,27 +7,23 @@
 */
 int main(void)
 {
-	int nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	int i;
-	int j;
-
-	for (i = 0; i < 10; i++)
+	int digit1, digit2;
+	
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (j = 0; j < 10; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar(nums[i]);
-			putchar(nums[j]);
-			if ((nums[i] == '9') && (nums[j] == '9'))
-			{
-			}
-			else
-			{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
 				putchar(',');
 				putchar(' ');
-			}
 		}
 	}
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
